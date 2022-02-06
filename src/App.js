d1fd901e08f0f73f9e1c512c153cd0782d0c4c7b
Anas Saoui <62400765+Anas-debug@users.noreply.github.com>
2022-02-06 03:44:33 +0100

    Created two components Main and Nav and added some styling.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import React from "react"
+import Navbar from "./components/Navbar"
 
 export default function App() {
     return (
     <>
+        <Navbar />
         <h1>Hello World!</h1>
-        <p>React is one of the most common JS libraries out there!</p>
+        <ul>
+            <li>React is one the most popular libraries in JavaScript.</li>
+            <li>Knowing React is very hireable skill.</li>
+            <li>React is fun to learn.</li>
+        </ul>
     </>
     )
 }
