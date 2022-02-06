9ba4bd7f673862d74ec1c397a7e33facfc2a972d
Anas Saoui <62400765+Anas-debug@users.noreply.github.com>
2022-02-06 03:52:09 +0100

    adjusted the navbar and made space for the icon

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,6 +3,7 @@ import React from 'react';
 export default function Navbar() {
     return(
         <nav class ="navbar">
+            <div class = "icon"></div>
             <ul class= "nav-list">
                 <li class= "list-el">Home</li>
                 <li class= "list-el">Pricing</li>
