86b831492abde05153160ad4398dbe0967e6de5d
Anas Saoui <62400765+Anas-debug@users.noreply.github.com>
2022-02-06 03:44:00 +0100

    "Created a Main and Nav components and added some styling

---
+++ src/components/Navbar.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export default function Navbar() {
+    return(
+        <nav class ="navbar">
+            <ul class= "nav-list">
+                <li class= "list-el">Home</li>
+                <li class= "list-el">Pricing</li>
+                <li class= "list-el">Contact</li>
+            </ul>
+        </nav>
+    )
+}
