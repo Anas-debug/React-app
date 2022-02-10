4913e1b7b5debee802da79ecaf2e3a392983fecc
Anas Saoui <62400765+Anas-debug@users.noreply.github.com>
2022-02-10 14:15:14 +0100

    Added the Main.js component.

--- src/App.js
+++ src/App.js
@@ -1,17 +1,13 @@
-import React from "react"
-import Navbar from "./components/Navbar"
-import Footer from "./components/Footer"
+import React from "react";
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
+import Main from "./components/Main";
 
 export default function App() {
     return (
     <>
         <Navbar />
-        <h1>React Library 16.8</h1>
-        <ul>
-            <li>React is one the most popular libraries in JavaScript.</li>
-            <li>Knowing React is very hireable skill.</li>
-            <li>React is fun to learn.</li>
-        </ul>
+        <Main />
         <Footer />
     </>
     )
---
+++ src/components/Main.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+export default function Main(){
+    return(
+        <main>
+        <h1>React Library 16.8</h1>
+        <ul>
+            <li>React is one the most popular libraries in JavaScript.</li>
+            <li>Knowing React is very hireable skill.</li>
+            <li>React is fun to learn.</li>
+        </ul>
+        </main>
+    )
+}
